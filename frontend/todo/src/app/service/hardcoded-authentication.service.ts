55fa6d30ff90aaf4b5814b50761325ab402a8212
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-05-06 18:26:03 +0530

    Added logout component

--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -25,4 +25,9 @@ export class HardcodedAuthenticationService {
     let user = sessionStorage.getItem('authenticatedUser');
     return !(user === null)
   }
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser');
+  }
+
 }
