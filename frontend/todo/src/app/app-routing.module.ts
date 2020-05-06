76fb33215a30ffacd76d711067e48c56e18f91e8
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-05-06 12:06:34 +0530

    Added error component and routing

--- frontend/todo/src/app/app-routing.module.ts
+++ frontend/todo/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 
 const routes: Routes = [
   { path:'welcome', component: WelcomeComponent},
   { path:'', component: LoginComponent},
   { path:'login', component: LoginComponent},
+  { path:'**', component: ErrorComponent},
 ];
 
 @NgModule({
